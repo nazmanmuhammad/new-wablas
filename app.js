f6806dda7f01e781e01daa56355335dd107c30ec
Nazman <nazmanmuhammad.business@gmail.com>
2022-07-28 14:49:48 +0700

    Added a Procfile.

--- app.js
+++ app.js
@@ -8,19 +8,23 @@ import cors from 'cors'
 const app = express()
 
 const host = process.env.HOST || undefined
-app.set('port', (process.env.PORT || 5000))
+const port = parseInt(process.env.PORT ?? 8000)
 
 app.use(cors())
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 app.use('/', routes)
 
-app.get('/', function(request, response) {
-    var result = 'App is running'
-    response.send(result);
-}).listen(app.get('port'), function() {
-    console.log('App is running, server is listening on port ', app.get('port'))
-});
+const listenerCallback = () => {
+    init()
+    console.log(`Server is listening on http://${host ? host : 'localhost'}:${port}`)
+}
+
+if (host) {
+    app.listen(port, host, listenerCallback)
+} else {
+    app.listen(port, listenerCallback)
+}
 
 nodeCleanup(cleanup)
 
