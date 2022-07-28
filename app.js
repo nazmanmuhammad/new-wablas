ea0be228ca3544fee4ae8b2ef236fd9eabec207e
Nazman <nazmanmuhammad.business@gmail.com>
2022-07-28 15:02:35 +0700

    ok

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ import routes from './routes.js'
 import { init, cleanup } from './whatsapp.js'
 import cors from 'cors'
 
-const express = required('express')
 const app = express()
 
 const host = process.env.HOST || undefined
