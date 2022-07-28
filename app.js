e965afc9086a0d386219153a188606be90765428
Nazman <nazmanmuhammad.business@gmail.com>
2022-07-28 12:31:50 +0700

    make it better

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.use('/', routes)
 
 const listenerCallback = () => {
     init()
-    console.log(`Server is listening on https://node-js-wa-blast.herokuapp.com`)
+    console.log(`Server is listening on http://${host ? host : 'localhost'}:${port}`)
 }
 
 if (host) {
